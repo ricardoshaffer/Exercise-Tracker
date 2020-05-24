5aa6337ecec04b9700ba29cf63adc42a72302e99
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-05-24 14:21:32 -0400

    modified html routes to have 'request' and 'response'versus 'req' and 'res'

--- routes/html.js
+++ routes/html.js
@@ -1,13 +1,13 @@
 var path = require("path");
 const router = require("express").Router();
 
-router.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+router.get("/", function(request, response) {
+  response.sendFile(path.join(__dirname, "../public/index.html"));
   });
-  router.get("/exercise", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  router.get("/exercise", function(request, response) {
+    response.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
-  router.get("/stats", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  router.get("/stats", function(request, response) {
+    response.sendFile(path.join(__dirname, "../public/stats.html"));
   });
   module.exports = router;
